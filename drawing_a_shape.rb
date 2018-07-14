efb9eeb7b29046d394a3b376425009bae8dfc73c
Unknown <talles_of_destiny@yahoo.com.sg>
2018-07-14 12:07:07 +0900

    Added 2nd lesson "Drawing a shape"

---
+++ drawing_a_shape.rb
@@ -0,0 +1,9 @@
+# this command "print" not return new line
+print "this print out a text" 
+# this command "puts" will have new line
+puts "this also print out a text"
+
+puts "   /|"
+puts "  / |"
+puts " /  |"
+puts "/___|"
